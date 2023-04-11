96972ac5a700a915e9ec82072a91c7efd4ad35f1
Dingkun Yang <dingkun.yang@duke.edu>
2023-04-10 21:21:14 -0400

    restructure test file

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,60 +2,59 @@
 from analysis import patient_age, patient_is_sick, parse_data
 from fake_files import fake_files
 
-with fake_files(
-    [
-        [
-            "PatientID",
-            "PatientGender",
-            "PatientDateOfBirth",
-            "PatientRace",
-            "PatientMaritalStatus",
-            "PatientLanguage",
-            "PatientPopulationPercentageBelowPoverty",
-        ],
-        [
-            "1",
-            "Male",
-            "1947-12-28 02:45:40.547",
-            "Unknown",
-            "Married",
-            "Icelandic",
-            "18.08",
-        ],
-        [
-            "2",
-            "Female",
-            "1970-07-25 13:04:20.717",
-            "Asian",
-            "Married",
-            "English",
-            "6.67",
-        ],
-    ],
-    [
-        [
-            "PatientID",
-            "AdmissionID",
-            "LabName",
-            "LabValue",
-            "LabUnits",
-            "LabDateTime",
-        ],
-        [
-            "1",
-            "1",
-            "METABOLIC: ALBUMIN",
-            "4.0",
-            "g/dL",
-            "2019-01-01 00:00:00.000",
-        ],
-    ],
-) as (patient_file, lab_file):
-    records = parse_data(patient_file, lab_file)
-
 
 def test_patient_data() -> None:
     """Test the patient_data function in analysis python."""
+    with fake_files(
+        [
+            [
+                "PatientID",
+                "PatientGender",
+                "PatientDateOfBirth",
+                "PatientRace",
+                "PatientMaritalStatus",
+                "PatientLanguage",
+                "PatientPopulationPercentageBelowPoverty",
+            ],
+            [
+                "1",
+                "Male",
+                "1947-12-28 02:45:40.547",
+                "Unknown",
+                "Married",
+                "Icelandic",
+                "18.08",
+            ],
+            [
+                "2",
+                "Female",
+                "1970-07-25 13:04:20.717",
+                "Asian",
+                "Married",
+                "English",
+                "6.67",
+            ],
+        ],
+        [
+            [
+                "PatientID",
+                "AdmissionID",
+                "LabName",
+                "LabValue",
+                "LabUnits",
+                "LabDateTime",
+            ],
+            [
+                "1",
+                "1",
+                "METABOLIC: ALBUMIN",
+                "4.0",
+                "g/dL",
+                "2019-01-01 00:00:00.000",
+            ],
+        ],
+    ) as (patient_file, lab_file):
+        records = parse_data(patient_file, lab_file)
     assert records == (
         {
             "1": {
@@ -90,16 +89,10 @@ def test_patient_data() -> None:
             ]
         },
     )
-
-
-def test_patient_age() -> None:
-    """ "Test the patient_age function in analysis python."""
+    # test the patient_age function in analysis python
     assert patient_age(records, "1") == 75
     assert patient_age(records, "2") == 52
-
-
-def test_patient_is_sick() -> None:
-    """Test the patient_is_sick function in analysis python."""
+    # test the patient_is_sick function in analysis python
     assert (
         patient_is_sick(records, "1", "METABOLIC: ALBUMIN", ">", 5.0) is False
     )
